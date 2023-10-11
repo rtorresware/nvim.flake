70b7fda3d4ddcf9241f10d7461996e4e99b6f137
Rodolfo Torres <rtorresware@gmail.com>
2023-10-11 14:56:32 -0400

    add htmldjango filetype to html lsp

--- init.lua
+++ init.lua
@@ -97,10 +97,15 @@ cmp.setup.cmdline(':', {
 
 -- Set up lspconfig.
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
-local servers = { 'pyright', 'tsserver', 'nil_ls', 'tailwindcss', 'html' }
+local servers = { 'pyright', 'tsserver', 'nil_ls', 'tailwindcss' }
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     on_attach = on_attach,
     capabilities = capabilities,
   }
 end
+lspconfig['html'].setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = { 'html', 'htmldjango' },
+}
